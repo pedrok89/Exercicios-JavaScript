c5d2be0f245fb3bb7ec29f0eab13b420c0494c18
Pedro Ivo Da Silva <35647447+pedrok89@users.noreply.github.com>
2022-10-19 20:42:36 -0300

    exercicios
    update

---
+++ teste/teste.js
@@ -0,0 +1,15 @@
+
+	document.getElementById('click').addEventListener('click', teste)
+
+	function teste() {
+
+		let balance = document.getElementById('balance').value
+
+		let basebet = document.getElementById('basebet').value
+
+		console.log(balance, basebet)
+
+	}
+
+	
+
